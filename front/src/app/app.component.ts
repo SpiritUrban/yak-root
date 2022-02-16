b747cfbb405dca05ebce457d39f6d9ca1d641274
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2022-02-16 11:07:52 +0200

    angular: move content from 'app.component' to 'dev' page

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -6,17 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'yak-root-front';
 
-  links = [
-    {link: 'http://localhost:3000/doge/1.jpg', title: 'Pic from public of backend: public/doge/1.jpg'},
-    {link: 'http://localhost:3000/doge/2.jpg', title: 'Pic from public of backend: public/doge/2.jpg'},
-    {link: 'http://localhost:3000/doge/3.jpg', title: 'Pic from public of backend: public/doge/3.jpg'},
-  ];
-
-  pages = [
-    {path: '', title: '/localhost'},
-    {path: '/x', title: '/localhost/x'},
-    {path: '/no-page', title: '/localhost/ololololololo'},
-  ];
 }
--- front/src/app/dev/dev.component.ts
+++ front/src/app/dev/dev.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dev.component.sass']
 })
 export class DevComponent implements OnInit {
+  title = 'yak-root-front';
 
+  links = [
+    {link: 'http://localhost:3000/doge/1.jpg', title: 'Pic from public of backend: public/doge/1.jpg'},
+    {link: 'http://localhost:3000/doge/2.jpg', title: 'Pic from public of backend: public/doge/2.jpg'},
+    {link: 'http://localhost:3000/doge/3.jpg', title: 'Pic from public of backend: public/doge/3.jpg'},
+  ];
+
+  pages = [
+    {path: '', title: '/localhost'},
+    {path: '/x', title: '/localhost/x'},
+    {path: '/no-page', title: '/localhost/ololololololo'},
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
