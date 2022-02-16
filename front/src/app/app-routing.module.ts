aee148e21a757f59a6f34a96bce8060abe9fe5af
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2022-02-16 11:03:12 +0200

    angular: add routing for 'dev' page

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { XComponent } from './x/x.component';
 import { MainComponent } from './main/main.component';
 import { P404Component } from './p404/p404.component';
+import { DevComponent } from './dev/dev.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: 'main', component: MainComponent },
   { path: 'x', component: XComponent },
+  { path: 'dev', component: DevComponent },
+  
   // 404
   { path: 'p404', component: P404Component },
   { path: '**', redirectTo: '/p404' }
