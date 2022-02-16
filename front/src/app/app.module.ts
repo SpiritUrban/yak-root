78da75d1fc97a1b0276cc2c13649e978efcfa07e
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2022-02-16 10:14:35 +0200

    Install Angular Material

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { XComponent } from './x/x.component';
 import { MainComponent } from './main/main.component';
 import { P404Component } from './p404/p404.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { P404Component } from './p404/p404.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
