7f128582c6402e8bb1467868e5857813ec1dc06a
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2022-02-22 00:13:19 +0200

    add routes and mechanic for correct switching navbar

--- front/src/app/parts/nav/nav.component.ts
+++ front/src/app/parts/nav/nav.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,6 +17,36 @@ export class NavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.demo(); // Just Demo Features!
+  }
+
+  // Just Demo Features!
+  demo(){
+    setInterval(() => {
+      console.log('Demo:', window.screen.width, window.innerWidth, '💋');
+    }, 1000);
+
+    this.route
+      .queryParams
+      .subscribe(params => {
+        let code = params['code'];
+        let userEmail = params['email'];
+        console.log('Route: ', code, userEmail)
+      });
+  }
+
+  goTo(path: string, drawer: any) {
+    console.log(path)
+    this.router.navigate([path]);
+    // ,{queryParams: {email: 'loginEmail', code: 'userCode'}} // can put it in to router
+    if (window.innerWidth < 600) drawer.toggle();
+  }
 
 }
